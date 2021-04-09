229695648d064152221b7fe518fb17bd3e4ae9de
sanjeelpn <myg7021@autuni.ac.nz>
2021-04-09 23:31:15 +1200

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js
+
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+const books = require('./routes/api/books');
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+app.use(cors({ origin: true, credentials: true }))
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
